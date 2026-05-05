2a46879975e645d1978474e1ca85bd1ecab83931
Irdi DOLONAKU <idolonaku@mgroup.local>
2026-05-05 13:59:20 +0200

    add build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './TouchDisplay_v3';
+import App from './TouchDisplay_v4';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
